f597bccc31ab2eff5a90c1bf1ee80dfde326964c
tesfa <alphatesfa789@gmail.com>
2020-10-05 13:25:48 +0300

    .

--- gateway/logic/logic.py
+++ gateway/logic/logic.py
@@ -36,7 +36,7 @@ class Logic:
         transaction = TransactionHeader.query.filter(TransactionHeader.uiid==tx_ref).first()
         transactionHeader_schema = TransactionHeaderSchema()
         data = transactionHeader_schema.dump(transaction)
-        data['uiid'] = data['uiid'][6:]
+        data = data['uiid']
         return data
 
     
