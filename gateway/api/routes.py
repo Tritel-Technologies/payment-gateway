c8d76b8b96bee1da4f5e8081f226375bd43837e1
tesfa-ozem <alphatesfa789@gmail.com>
2021-01-13 10:26:01 +0300

    [UPDATE] odoo url for call back

--- gateway/api/routes.py
+++ gateway/api/routes.py
@@ -65,9 +65,9 @@ def confirmation_callback():
             bill_ref=request.json['BillRefNumber'], uiid=request.json['BillRefNumber'])
         db.session.add(transaction)
         db.session.commit()
-        odoo_url = 'https://5dc8d52130a3.ngrok.io/payment/mpesa/callback'
+        odoo_url = 'http://c6675e44bf65.ngrok.io/payment/mpesa/callback'
         requests.post(
-            odoo_url, json=request.json['BusinessShortCode'])
+            odoo_url, json=request.json)
         # payment = MpesaTransaction(name=request.json['FirstName'], amount=request.json['TransAmount'],
         #                        phone_number=request.json['MSISDN'], bill_ref=request.json['BillRefNumber'],
         #                        transaction_id=request.json['TransID'])
