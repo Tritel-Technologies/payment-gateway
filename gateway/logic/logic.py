db5f3113fdbe526b2d288c85bb37e4c26a1a7ed3
tesfa-ozem <alphatesfa789@gmail.com>
2021-01-16 17:37:56 +0300

    [REMOVED] try catch

--- gateway/logic/logic.py
+++ gateway/logic/logic.py
@@ -63,13 +63,10 @@ class Logic:
         return data
 
     def post_to_odoo(self, payload):
-        try:
-            self.invoke('mpesa.payment.transaction',
-                        'create', [{'name': payload['BillRefNumber'],
-                                    'customer_name':f"{payload['FirstName']} {payload['LastName']}",
-                                    "amount":payload['TransAmount'],
-                                    'phone_number':payload['MSISDN'],
-                                    "reference":payload['InvoiceNumber'],
-                                    "payment_date":datetime.strftime(payload['TransTime'], '%Y%m%d%H%M%S')}])
-        except Exception as e:
-            print(e)
+        self.invoke('mpesa.payment.transaction',
+                    'create', [{'name': payload['BillRefNumber'],
+                                'customer_name':f"{payload['FirstName']} {payload['LastName']}",
+                                "amount":payload['TransAmount'],
+                                'phone_number':payload['MSISDN'],
+                                "reference":payload['InvoiceNumber'],
+                                "payment_date":datetime.strftime(payload['TransTime'], '%Y%m%d%H%M%S')}])
